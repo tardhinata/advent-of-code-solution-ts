b93f938c90a6375d8b3661202c47d5f769f67ffe
taufanardi <taufan.ardi@gmail.com>
2022-12-02 07:22:36 +0100

    added base class to handle console parameter

--- 2022/day1/index.ts
+++ 2022/day1/index.ts
@@ -3,18 +3,13 @@
  */
 
 import fs from 'fs';
+import { AdventOfCode } from '../../shared/adventOfCode';
 
-class Day1 {
-	private input: Array<Array<number>> = [];
-	private result: number | undefined;
+class Day1 extends AdventOfCode {
+	protected input: Array<Array<number>> = [];
+	protected result: number = 0;
 
-	/**
-	 * Process the input files.
-	 * @param {string} inputPath default is "p{part number}.input" file in the problem directory
-	 * @param {string} resultPath default is "p{part number}.result" file in the problem directory
-	 * @protected
-	 */
-	private processInput(inputPath: string, resultPath: string): void {
+	protected processInput(inputPath: string, resultPath: string): void {
 		let tempGroup: Array<number> = [];
 		fs.readFileSync(inputPath, 'utf8')
 			.split('\n')
@@ -31,36 +26,15 @@ class Day1 {
 		this.result = parseInt(fs.readFileSync(resultPath, 'utf8').trim());
 	}
 
-	public start() {
-		const args = process.argv.slice(2);
-		const part = args[0] || '1';
-		const sourcePath = args[1] || `p${part}.input`;
-		const resultPath = `p${part}.result`;
-
-		this.processInput(sourcePath, resultPath);
-
-		const output = part === '1' ? this.part1() : this.part2();
-		console.debug('Solution', output);
-
-		if (args.length < 2) { //second argument is to pass the real data source, thus no need to print the test result.
-			console.debug('Expected', this.result);
-			if (this.result === output) {
-				console.log(`Passed!`);
-			} else {
-				console.log(`Failed!`);
-			}
-		}
-	}
-
-	private part1(): number {
-		return this.input.reduce((acc, group) => {
-			const currCalories = group.reduce((acc, cur) => acc + cur, 0); //summarize all calories per elf
+	protected part1(): number {
+		return this.input.reduce((acc, currGroup) => {
+			const currCalories = currGroup.reduce((acc, cal) => acc + cal, 0); //summarize all calories per elf
 			return Math.max(currCalories, acc); //return the biggest number
 		}, 0);
 	}
 
-	private part2(): number {
-		return this.input.map(group => group.reduce((acc, cur) => acc + cur, 0)) //summarize all calories per elf
+	protected part2(): number {
+		return this.input.map(group => group.reduce((acc, cal) => acc + cal, 0)) //summarize all calories per elf
 			.sort((a, b) => b - a) //sort ascending from big to low
 			.slice(0, 3) //take the top 3
 			.reduce((acc, cur) => acc + cur, 0); //summarize the top 3
---
+++ shared/adventOfCode.ts
@@ -0,0 +1,35 @@
+
+export abstract class AdventOfCode {
+  protected abstract input: Array<Array<string | number>>;
+  protected abstract result: string | number;
+  /**
+   * Process the input files.
+   * @param {string} inputPath default is "p{part_number}.input" file in the problem directory
+   * @param {string} resultPath default is "p{part_number}.result" file in the problem directory
+   * @protected
+   */
+  protected abstract processInput(inputPath: string, resultPath: string): void;
+  protected abstract part1(): string | number;
+  protected abstract part2(): string | number;
+
+  public start() {
+    const args = process.argv.slice(2);
+    const part = args[0] || '1';
+    const sourcePath = args[1] || `p${part}.input`;
+    const resultPath = `p${part}.result`;
+
+    this.processInput(sourcePath, resultPath);
+
+    const output = part === '1' ? this.part1() : this.part2();
+    console.debug('Solution', output);
+
+    if (args.length < 2) { //second argument is the real input source, thus if provided, no need to print the expected result.
+      console.debug('Expected', this.result);
+      if (this.result === output) {
+        console.log(`Passed!`);
+      } else {
+        console.log(`Failed!`);
+      }
+    }
+  }
+}
