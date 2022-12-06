712235b5a003bbbb6aa6586ba07f08539833081c
Taufan Ardhinata <taufan.ardi@gmail.com>
2022-12-06 06:27:47 +0100

    added solution for day 6

---
+++ 2022/day6/index.ts
@@ -0,0 +1,35 @@
+/**
+ * Solution for https://adventofcode.com/2022/day/5
+ */
+
+import fs from 'fs';
+import { AdventOfCode } from '../../shared/adventOfCode';
+
+class Day6 extends AdventOfCode {
+  protected input: Array<string> = [];
+  protected result: number = 0;
+
+  protected processInput(inputPath: string, resultPath: string): void {
+    this.input = fs.readFileSync(inputPath, 'utf8').trim().split('');
+    this.result = parseInt(fs.readFileSync(resultPath, 'utf8').trim());
+  }
+
+  private findMarker(markerLength: number): number {
+    return this.input.findIndex((char, idx) => {
+      if (idx >= markerLength) {
+        const marker = this.input.slice(idx - markerLength, idx);
+        return marker.length === new Set(marker).size;
+      }
+    });
+  }
+
+  protected part1(): number {
+    return this.findMarker(4);
+  }
+
+  protected part2(): number {
+    return this.findMarker(14);
+  }
+}
+
+new Day6().start();
